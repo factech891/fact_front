35ce5aeb9284fca61cd6529625b1e12544a7b0dc
Richard Garcia <richardgg646@gmail.com>
2025-02-07 22:59:29 -0300

    Mejoras modulos

--- src/components/Invoices/components/PaymentDetails/styles.js
+++
@@ -1,6 +0,0 @@
-// components/PaymentDetails/styles.js
-export const styles = {
-    container: {
-        marginBottom: 2
-    }
- };
--- src/components/Invoices/components/ProductsTable/styles.js
+++
@@ -1,9 +0,0 @@
-// components/ProductsTable/styles.js
-export const styles = {
-    table: {
-        minWidth: 650,
-    },
-    tableCell: {
-        padding: '8px',
-    }
-};
